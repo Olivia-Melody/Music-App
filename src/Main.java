72278474bc814a29709a7e6bd626979443e5fbdd
Olivia-Melody <livmelyin@gmail.com>
2025-03-26 23:43:43 +0000

    sample song class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.jar.Manifest;
 
 public class MusicLibrary {
     private MusicLibrary library; // Use MusicLibrary instead of storing songs directly
@@ -6,3 +7,29 @@ public class MusicLibrary {
     public MusicStreamingApp() {
         library = new MusicLibrary(); // Initialize music library
     }
+
+    public static void main(String[] args) {
+        Main app = new Main();
+        Scanner scanner = new Scanner(System.in);
+
+        // Adding sample songs
+        app.library.addSong("Song A", "Artist X", 120);
+        app.library.addSong("Song B", "Artist Y", 85);
+        app.library.addSong("Song C", "Artist Z", 300);
+        app.library.addSong("Song D", "Artist X", 150);
+
+        while (true) {
+            System.out.println("\nMusic App Menu:");
+            System.out.println("1. Add a new song");
+            System.out.println("2. Remove a song");
+            System.out.println("3. Print all songs");
+            System.out.println("5. Exit");
+            System.out.print("Choose an option: ");
+
+
+
+
+
+    }
+    }
+}
